17efcf7c0f9ec672a9ee5b52b623976e0cc3ade7
Regino P <regino5298@gmail.com>
2019-12-13 10:20:05 -0700

    added Form, Smurf, and SmurfList

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getSmurfs } from './actions';
+import Smurf from './Smurf';
+import Form from './Form';
+
+const SmurfList = (props) => {
+  console.log(props)
+
+  useEffect(() => {
+    props.getSmurfs();
+  }, [])
+
+  return (
+    <div>
+      <h2>Smurf residents: </h2>
+      {props.state.map(smurf => (
+        <Smurf key={smurf.id} smurf={smurf} />
+      ))}
+      <Form />
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return{
+    state
+  }
+}
+
+export default connect(mapStateToProps, {getSmurfs})(SmurfList);
