48869f6a6bb04a25677ba90fdcd8c87eddd6daa8
Regino P <regino5298@gmail.com>
2019-12-13 09:51:53 -0700

    added reducer component

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,1 +1,18 @@
 import { SMURFS_LOAD_SUCCESS, SMURF_ADD_SUCCESS, } from './actions'
+
+const initialState = [];
+
+export const AppReducer = (state = initialState, action) => {
+  console.log(state);
+  switch (action.type) {
+    case SMURFS_LOAD_SUCCESS:
+      return [...state, ...action.payload];
+
+    case SMURF_ADD_SUCCESS:
+      console.log(action.payload);
+      return [...state, action.payload];
+
+    default:
+      return state;
+  }
+};
